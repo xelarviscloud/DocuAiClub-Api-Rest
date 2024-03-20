625a23f909d028f01d6ebccbf0fa2bb1e36d6052
HG <ghori_hitesh@yahoo.com>
2024-03-19 21:52:09 -0500

    fix(env)

--- src/database/connection-mdb.js
+++ src/database/connection-mdb.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-// Configure environment variables
 dotenv.config();
 
-const databaseConnection =
-  "mongodb+srv://staysolveadmin:qA9FyrjXJt*Er2$@stay-solve-database.sni6cfi.mongodb.net/stay-solve-backend";
+const databaseConnection = process.env.DATABASE_CONNECTION;
+//"mongodb+srv://staysolveadmin:qA9FyrjXJt*Er2$@stay-solve-database.sni6cfi.mongodb.net/stay-solve-backend";
 
-// DB connection function
 mongoose
   .connect(databaseConnection)
   .then(() => {
--- src/healthcheck/mongodb-healthcheck.js
+++ src/healthcheck/mongodb-healthcheck.js
@@ -1,16 +1,13 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const databaseConnection =
-  "mongodb+srv://staysolveadmin:qA9FyrjXJt*Er2$@stay-solve-database.sni6cfi.mongodb.net/stay-solve-backend";
-
 const databaseRouter = express.Router();
 
 databaseRouter.get("/v2/database/healthcheck", async (req, res) => {
-  res.send("connectionUrl: " + process.env.DATABASE_CONNECTION);
+  //res.send("connectionUrl: " + process.env.DATABASE_CONNECTION);
+  res.send({ Message: "database health check okay!" });
 });
 
 export default databaseRouter;
